7a4778ecb22b7b7ead4374ebbdec504fd15468b3
syq0806 <61413903+syq0806@users.noreply.github.com>
2020-03-30 12:14:54 +0800

    Update koolproxyupdate.sh

--- koolproxyupdate.sh
+++ koolproxyupdate.sh
@@ -2,7 +2,7 @@ wget 'https://houzi-.coding.net/p/my_dream/d/my_dream/git/raw/master/kp.dat' -O 
 wget 'https://houzi-.coding.net/p/my_dream/d/my_dream/git/raw/master/koolproxy.txt' -O files/usr/share/koolproxy/data/rules/koolproxy.txt
 wget 'https://houzi-.coding.net/p/my_dream/d/my_dream/git/raw/master/daily.txt' -O files/usr/share/koolproxy/data/rules/daily.txt
 wget 'https://houzi-.coding.net/p/my_dream/d/my_dream/git/tree/master/user.txt' -O files/usr/share/koolproxy/data/user.txt
-#wget 'https://koolproxy.com/downloads/arm' -O files/bin/arm
+wget 'https://github.com/syq0806/luci-app-koolproxy/blob/master/files/bin/aarch64' -O files/bin/aarch64
 #wget 'https://koolproxy.com/downloads/i386' -O files/bin/i386
 #wget 'https://koolproxy.com/downloads/mips' -O files/bin/mips
 #wget 'https://koolproxy.com/downloads/mipsel' -O files/bin/mipsel
